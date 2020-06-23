5842cd32ac8bb2d3ba72bb1c2617bbbe06ec2f55
ApostolisNt <apo7ntask@gmail.com>
2020-06-23 18:55:15 +0300

    send receipt for printing with Okhttp

--- app/src/main/java/com/example/orderit/CartActivity.java
+++ app/src/main/java/com/example/orderit/CartActivity.java
@@ -5,27 +5,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.spark.submitbutton.SubmitButton;
-
-import java.io.IOException;
-import java.util.Objects;
-import java.util.concurrent.Executors;
-
-import okhttp3.Call;
-import okhttp3.FormBody;
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
 
 public class CartActivity extends AppCompatActivity {
 
